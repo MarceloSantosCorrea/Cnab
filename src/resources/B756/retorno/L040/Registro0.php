6c5f83450493833ae8bb1214456b664d3f1b21c8
Marcelo Corrêa <marcelocorrea229@gmail.com>
2018-12-08 12:20:10 -0200

    ...

--- src/resources/B756/retorno/L040/Registro0.php
+++ src/resources/B756/retorno/L040/Registro0.php
@@ -23,7 +23,7 @@
 * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
-namespace Cnab\resources\B341\retorno\L040; // SIGCB
+namespace Cnab\resources\B756\retorno\L040; // SIGCB
 use Cnab\resources\generico\retorno\L040\Generico0;
 use Cnab\AbstractRetorno;
 
--- src/resources/B756/retorno/L040/Registro9.php
+++ src/resources/B756/retorno/L040/Registro9.php
@@ -23,7 +23,7 @@
  * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-namespace Cnab\resources\B341\retorno\L040;
+namespace Cnab\resources\B756\retorno\L040;
 use Cnab\resources\generico\retorno\L040\Generico9;
 use Cnab\Exception;
 
