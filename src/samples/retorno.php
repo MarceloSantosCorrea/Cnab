ece120dabbd8fad397b1babe3b6ead5a62b47c73
Marcelo Corrêa <marcelocorrea229@gmail.com>
2019-01-25 18:42:34 -0200

    ...

--- src/samples/retorno.php
+++ src/samples/retorno.php
@@ -5,8 +5,8 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 use Cnab\Retorno;
 
 
-$fileContent = file_get_contents('src/samples/retorno_cnab400_itau.ret');
-//$fileContent = file_get_contents('src/samples/phpyoiOl5_5a9011f5495133_12170818.ret');
+//$fileContent = file_get_contents('src/samples/retorno_cnab400_itau.ret');
+$fileContent = file_get_contents('src/samples/phpyoiOl5_5a9011f5495133_12170818.ret');
 //$fileContent = file_get_contents('src/samples/retorno-000021.ret');
 
 try {
