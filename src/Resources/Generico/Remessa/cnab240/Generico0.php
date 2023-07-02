72a9a356b70f3076e1735a6738a2c43cdae79980
Marcelo Corrêa <marcelocorrea229@gmail.com>
2023-07-02 20:02:08 -0300

    alterando psr-4

--- src/Resources/Generico/Remessa/cnab240/Generico0.php
+++ src/Resources/Generico/Remessa/cnab240/Generico0.php
@@ -16,7 +16,7 @@ class Generico0 extends AbstractRegistroRemessa
 
     protected function set_situacao_arquivo($value)
     {
-        $this->data['situacao_arquivo'] = ($value == 'T') ? "REMESSA-TESTE" : "REMESSA-PRODUCAO";
+        $this->data['situacao_arquivo'] = ($value == 'T') ? 'REMESSA-TESTE' : 'REMESSA-PRODUCAO';
     }
 
     protected function set_data_geracao($value)
@@ -34,7 +34,7 @@ class Generico0 extends AbstractRegistroRemessa
         if ($value == 1 || $value == 2) {
             $this->data['tipo_inscricao'] = $value;
         } else {
-            throw new Exception("O tipo de incrição deve ser 1  para CPF e 2 para CNPJ, o valor informado foi:" . $value);
+            throw new Exception('O tipo de incrição deve ser 1  para CPF e 2 para CNPJ, o valor informado foi:' . $value);
         }
     }
 
@@ -42,5 +42,4 @@ class Generico0 extends AbstractRegistroRemessa
     {
         $this->data['numero_inscricao'] = str_ireplace(['.', '/', '-'], [''], $value);
     }
-
 }
